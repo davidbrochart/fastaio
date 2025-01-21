f9a390bcd379a35a75d8a7c128523cd2d7346dc8
David Brochart <david.brochart@gmail.com>
2025-01-21 07:30:16 +0100

    Change port between test runs

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,5 +1,6 @@
 import pytest
 
+import httpx
 from anyio import connect_tcp, create_task_group
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
@@ -8,14 +9,12 @@ from fastaio import Component
 from fastaio.web.fastapi import FastAPIComponent
 
 pytestmark = pytest.mark.anyio
+port = 8000 - 1
 
 
 async def test_web():
-    while True:
-        try:
-            await connect_tcp("127.0.0.1", 8000)
-        except OSError:
-            break
+    global port
+    port += 1
 
     class Subcomponent0(Component):
         async def prepare(self):
@@ -32,13 +31,13 @@ async def test_web():
     class Component0(Component):
         def __init__(self, name):
             super().__init__(name)
-            self.add_component(FastAPIComponent, "fastapi_component")
+            self.add_component(FastAPIComponent, "fastapi_component", port=port)
             self.add_component(Subcomponent0, "subcomponent0")
 
     component0 = Component0("component0")
 
     async with component0:
         app = component0.components["subcomponent0"].app
-        with TestClient(app) as client:
-            response = client.get("/")
-            assert response.json() == {"Hello": "World"}
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f"http://127.0.0.1:{port}")
+        assert response.json() == {"Hello": "World"}
